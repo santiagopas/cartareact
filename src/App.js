646815b110ef9177cae2ee61856d04fc93e6c695
Santiago Pasciullo <santiagopasciullo@gmail.com>
2021-08-31 23:08:17 -0300

    carta con react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css';
+// import p1 from './assets/img/1.jpg'
+// import p2 from './assets/img/2.jpg'
+// import p3 from './assets/img/3.jpg'
+// import p4 from './assets/img/4.jpg'
+// import p5 from './assets/img/5.jpg'
+// import p6 from './assets/img/6.jpg'
+// import p7 from './assets/img/7.jpg'
+// import p8 from './assets/img/8.jpg'
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="Wrapper">
+      
+//         <img src={p1} width="640" height="360" className="Pagina" alt="pagina 1" />
+//         <img src={p2} width="640" height="360" className="Pagina" alt="pagina 2" />
+//         <img src={p3} width="640" height="360" className="Pagina" alt="pagina 3" />
+//         <img src={p4} width="640" height="360" className="Pagina" alt="pagina 4" />
+//         <img src={p5} width="640" height="360" className="Pagina" alt="pagina 5" />
+//         <img src={p6} width="640" height="360" className="Pagina" alt="pagina 6" />
+//         <img src={p7} width="640" height="360" className="Pagina" alt="pagina 7" />
+//         <img src={p8} width="640" height="360" className="Pagina" alt="pagina 8" /> 
+//       </div>
+//     </div>
+//   );
+// }
+// export default App;
+
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+const allCategories = ['todos', ...new Set(items.map((item) => item.category))];
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'todos') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Bar Consultorio</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,377 @@
+const menu = [
+  {
+    id: 1,
+    title: 'Amaretto Sour',
+    category: 'Vademécum',
+    price: 260,
+    img:'../assets/img/item-1.png',
+    desc: `Licor de amaretto, Jugo de limón, clara de huevo/aquafaba.`,
+  },
+  {
+    id: 2,
+    title: 'Black Russian',
+    category: 'Vademécum',
+    price: 300,
+    desc: `Vodka Smirnoff, licor Borghetti y granos de café.`,
+  },
+  {
+    id: 3,
+    title: 'Blue Blazer',
+    category: 'Vademécum',
+    price: 360,
+    desc: `Johnnie Walker Red Label, almíbar, agua hirviendo, twist de limón/naranja.`,
+  },
+  {
+    id: 4,
+    title: 'Boulevardier',
+    category: 'Vademécum',
+    price: 360,
+    desc: `Whiskey Bourbon, bitter rojo , vermut rosso, slice de naranja.`,
+  },
+  {
+    id: 5,
+    title: 'Fernet con cola',
+    category: 'Vademécum',
+    price: 300,
+    desc: `Fernet Branca con gaseosa coca cola .`,
+  },
+  {
+    id: 6,
+    title: 'Caipiroska',
+    category: 'Vademécum',
+    price: 310,
+    desc: `Vodka Smirnoff, jugo de lima y almíbar.
+    Variación: Caipirinha.`,
+  },
+  {
+    id: 7,
+    title: 'Captain Levi',
+    category: 'Vademécum',
+    price: 330,
+    desc: `Whisky irlandés, fernet branca, almíbar simple y menta.`,
+  },
+  {
+    id: 8,
+    title: 'Cream dream',
+    category: 'Vademécum',
+    price: 330,
+    desc: `Amarula, Ron añejo, crema, Rayadura de cacao.`,
+  },
+  {
+    id: 9,
+    title: 'Cuba libre',
+    category: 'Vademécum',
+    price: 300,
+    desc: `Ron blanco, gaseosa cola, jugo y gajo/piel de limón.`,
+  },
+  {
+    id: 10,
+    title: 'Cynar julep',
+    category: 'Vademécum',
+    price: 260,
+    desc: `Cynar 70, jugo de lima, menta, azúcar y jugo de pomelo.`,
+  },
+  {
+    id: 11,
+    title: 'Dry Martini',
+    category: 'Vademécum',
+    price: 290,
+    desc: 'Gin Gordon´s, vermut dry, piel de limón, aceituna.'
+  },
+  {
+    id: 12,
+    title: 'Gin Tonic',
+    category: 'Vademécum',
+    price: 300,
+    desc: 'Gin Gordon´s, tónica, piel de limón (y/o tus botánicos favoritos).'
+  },
+  {
+    id: 13,
+    title: 'God Father',
+    category: 'Vademécum',
+    price: 320,
+    desc: 'Johnnie Walker Red label y licor de amaretto'
+  },
+  {
+    id: 14,
+    title: 'Hanky Panky',
+    category: 'Vademécum',
+    price: 300,
+    desc: 'Gin Gordon´s, vermut rosso, fernet y twist de naranja.'
+  },
+  {
+    id: 15,
+    title: 'Long island ice tea',
+    category: 'Vademécum',
+    price: 380,
+    desc: 'Gin Gordon´s, tequila, vodka Smirnoff, ron blanco, triple sec, almíbar, jugo de limón, gaseosa cola, slice de limón.'
+  },
+  {
+    id: 16,
+    title: 'Manhattan',
+    category: 'Vademécum',
+    price: 360,
+    desc: 'Whiskey, vermut rosso, bitter aromático y cereza.'
+  },
+  {
+    id: 17,
+    title: 'Margarita',
+    category: 'Vademécum',
+    price: 290,
+    desc: 'Tequila, triple sec, jugo de lima, almíbar.'
+  },
+  {
+    id: 18,
+    title: 'Martínez',
+    category: 'Vademécum',
+    price: 290,
+    desc: 'Gin Gordon’s, vermut dry, maraschino, bitter aromático, piel de lima.'
+  },
+  {
+    id: 19,
+    title: 'Mojito',
+    category: 'Vademécum',
+    price: 300,
+    desc: 'Ron blanco, lima, almíbar, menta, soda. '
+  },
+  {
+    id: 20,
+    title: 'Moscow Mule',
+    category: 'Vademécum',
+    price: 290,
+    desc: 'Vodka Smirnoff, jugo de lima, ginger ale.'
+  },
+  {
+    id: 21,
+    title: 'Negroni',
+    category: 'Vademécum',
+    price: 290,
+    desc: 'Gin Gordon’s, bitter rojo, vermut rosso.'
+  },
+  {
+    id: 22,
+    title: 'Old Fashioned',
+    category: 'Vademécum',
+    price: 360,
+    desc: 'Whiskey, bitter aromático, azúcar, slice de naranja, cereza.'
+  },
+  {
+    id: 23,
+    title: 'Paloma',
+    category: 'Vademécum',
+    price: 300,
+    desc: 'Tequila, jugo de lima, pomelo.'
+  },
+  {
+    id: 24,
+    title: 'Penicillin',
+    category: 'Vademécum',
+    price: 290,
+    desc: 'Whisky escocés, jugo de limón, jengibre, miel.'
+  },
+  {
+    id: 25,
+    title: 'Piña colada',
+    category: 'Vademécum',
+    price: 290,
+    desc: 'Ron blanco, ananá, crema de coco, lima, almíbar.'
+  },
+  {
+    id: 26,
+    title: 'Pisco sour',
+    category: 'Vademécum',
+    price: 260,
+    desc: 'Pisco, Jugo de lima, almíbar, clara de huevo, bitter’s.'
+  },
+  {
+    id: 27,
+    title: 'Presidente',
+    category: 'Vademécum',
+    price: 260,
+    desc: 'Ron blanco, vermut bianco, triple sec, granadina, piel de naranja.'
+  },
+  {
+    id: 28,
+    title: 'Ramoz Gin Fizz',
+    category: 'Vademécum',
+    price: 380,
+    desc: 'Gin Gordon’s, almíbar, jugo de lima, jugo de limón, crema, clara de huevo, dashes de agua de azahar, vainilla, soda, piel de naranja.'
+  },
+  {
+    id: 29,
+    title: 'Rob Roy',
+    category: 'Vademécum',
+    price: 300,
+    desc: 'Johnnie Walker Red Label, vermut rosso, bitter aromático y cereza.'
+  },
+  {
+    id: 30,
+    title: 'Sazerac',
+    category: 'Vademécum',
+    price: 290,
+    desc: 'Coñac, absenta, almíbar, bitter aromático, piel de limón.'
+  },
+  {
+    id: 31,
+    title: 'Sex on the Beach',
+    category: 'Vademécum',
+    price: 320,
+    desc: 'Vodka Smirnoff, Licor de Durazno, Jugo de lima, Jugo de naranja, arándanos y almíbar.'
+  },
+  {
+    id: 32,
+    title: 'Spritz',
+    category: 'Vademécum',
+    price: 280,
+    desc: 'Aperol Spritz, espumante, soda, rodaja de naranja.'
+  },
+  {
+    id: 33,
+    title: 'T.N.T.',
+    category: 'Vademécum',
+    price: 330,
+    desc: 'Tequila, agua tónica, piel de limón.'
+  },
+  {
+    id: 34,
+    title: 'Tom Collins',
+    category: 'Vademécum',
+    price: 350,
+    desc: 'Gin Old Tom, Jugo de limón, azúcar, soda, bitter.'
+  },
+  {
+    id: 35,
+    title: 'Wiskey sour',
+    category: 'Vademécum',
+    price: 400,
+    desc: 'Whiskey bourbon, jugo de limón, almíbar.'
+  },
+  {
+    id: 36,
+    title: 'White lady',
+    category: 'Vademécum',
+    price: 280,
+    desc: 'Gin Gordon’s, triple sec, jugo de limón, piel de limón.'
+  },
+  {
+    id: 37,
+    title: 'White russian',
+    category: 'Vademécum',
+    price: 320,
+    desc: 'Vodka Smirnoff, Borghetti, crema, granos de café.'
+  },
+  {
+    id: 38,
+    title: 'Rook (Br. Maximiliano Aguirre)',
+    category: 'Vademécum',
+    price: 350,
+    desc: 'Ron añejo, Borghetti, amaretto, malibu, crema y cacao.'
+  },
+  {
+    id: 39,
+    title: 'Elixir',
+    category: 'Fórmulas-magistrales',
+    price: 330,
+    desc: 'Absenta, coñac, miel, jugo de mandarina, jugo de lima, aquafaba, ahumado.'
+  },
+  {
+    id: 40,
+    title: 'Dr. Silvyus',
+    category: 'Fórmulas-magistrales',
+    price: 310,
+    desc: 'Branca menta, ginebra, punt e mes, jugo de naranja, jugo de lima, orange bitter, pimienta.'
+  },
+  {
+    id: 41,
+    title: 'Serendipity',
+    category: 'Fórmulas-magistrales',
+    price: 390,
+    desc: 'Gin Gordon’s, hesperidina, licor de cassis, jugo de naranja y lima.'
+  },
+  {
+    id: 42,
+    title: 'Hechizo de las 100 primaveras',
+    category: 'Fórmulas-magistrales',
+    price: 330,
+    desc: 'Gin Gordon’s, liquore Strega, jugo de limón, naranja, azahar, jengibre'
+  },
+  {
+    id: 43,
+    title: 'Electuario',
+    category: 'Fórmulas-magistrales',
+    price: 290,
+    img: './images/item-1.png',
+    desc: 'Absenta, Pineral, jugo de naranja, almíbar de miel.'
+  },
+  {
+    id: 44,
+    title: 'Diga 33',
+    category: 'Fórmulas-magistrales',
+    price: 310,
+    desc: 'Grappa, apricot brandy, jugo de naranja.'
+  },
+  {
+    id: 45,
+    title: 'Clonazepam',
+    category: 'Fórmulas-magistrales',
+    price: 310,
+    desc: 'Gin Gordon’s, jengibre, jugo de limón, bitter de naranja, slice de pepino.'
+  },
+  {
+    id: 46,
+    title: 'Aspirineta',
+    category: 'Fórmulas-magistrales',
+    price: 310,
+    desc: 'Whisky, almíbar, Jugo de naranja, bitter aromático, piel de naranja.'
+  },
+  {
+    id: 47,
+    title: '2 cada 8hs',
+    category: 'Fórmulas-magistrales',
+    price: 330,
+    desc: 'Ron blanco, vermut bianco, almíbar de miel, jugo de naranja, lima.'
+  },
+  {
+    id: 48,
+    title: 'Jarabe para la tos',
+    category: 'Fórmulas-magistrales',
+    price: 330,
+    desc: 'Tequila, licor de cassis, pomelo, lima, menta.'
+  },
+  {
+    id: 49,
+    title: 'Terapia de media noche',
+    category: 'Fórmulas-magistrales',
+    price: 350,
+    desc: 'Los intocables Malbec, Whisky Escocés, Triple sec, Punt e mes, Jugo de naranja, lima, almíbar, cardamomo, clavo de olor, canela y menta.'
+  },
+  {
+    id: 50,
+    title: 'Valium',
+    category: 'Fórmulas-magistrales',
+    price: 350,
+    desc: 'Absenta, vermut bianco, Gin Gordon’s, truple sec, jugo de lima, aquafaba y anís estrellado.'
+  },
+  {
+    id: 51,
+    title: 'Ritalin',
+    category: 'Fórmulas-magistrales',
+    price: 350,
+    desc: 'Absenta,  Brandy, Almíbar de IPA, Jugo de naranja, bitter’s y twist de naranja.'
+  },
+  {
+    id: 52,
+    title: 'Remedio Escocés',
+    category: 'Fórmulas-magistrales',
+    price: 380,
+    desc: 'Johnnie Walker Red, almíbar de mascabo, canela y cayena, Jugo de limón, Piel de limón y canela.'
+  },
+  {
+    id: 53,
+    title: 'BBQ Chicken Tendedr´s',
+    category: 'Comidas',
+    price: 380,
+    desc: 'Tenders rebozados con salsa BBQ.'
+  },
+]
+export default menu;
